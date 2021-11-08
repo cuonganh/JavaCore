314c662ddaf50b159eb6fc335e0779e4740cd631
CuongTV <46061394+cuonganh@users.noreply.github.com>
2021-11-08 15:57:56 +0700

    Push the pseudocode of the Triangle Checker program

---
+++ Assignment02/Triangle.java
@@ -0,0 +1,35 @@
+public class Triangle {
+    /*
+     * pseudocode 
+     * 
+     * START 
+     * 
+     * PROGRAM TriangleChecker 
+     * 
+     * Create variables: num1, num2, num3 
+     * 
+     * DO
+     * Ask the user for num1, num2, num3
+     * READ INPUT into num1, num2, num3
+     * 
+     * Create variables: keyScanner
+     * Ask the user do you want to continue
+     * READ INPUT into keyScanner
+     * IF keyScanner means Yes
+     * Check the number num1, num2, num3 is a triangle
+     * IF the sum of any 2 numbers is greater than the other
+     * PRINT they are a triangle
+     * ELSE
+     * PRINT they are not a triangle
+     * EXIT proram
+     * WHILE true
+     * ENDWHILE
+     * 
+     * END
+     * 
+     */
+
+     public static void TriangleChecker() {
+         
+     }
+}
