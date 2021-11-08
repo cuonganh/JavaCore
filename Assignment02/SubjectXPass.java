b8683f5bc1f354a02ba52660a4d14a8316717602
CuongTV <46061394+cuonganh@users.noreply.github.com>
2021-11-08 15:20:29 +0700

    Task 1 and 2 are finish
    - Finish task 1: input all  the list number of students
    - Finish task 2 with 2 options:
    + Option 1: limit input the number of students with loops and ask the user to continue input
    + Option 2: unlimited input the number of students with loops and the user to continue input

--- Assignment02/SubjectXPass.java
+++ Assignment02/SubjectXPass.java
@@ -1,41 +1,42 @@
-// import java.util.Scanner;
 
-import java.util.*;
+
+import java.util.Scanner;
 
 public class SubjectXPass {
-    private static boolean passedHurdle;
 
-    public static void main(String[] args) {
+    static Scanner keyboard = new Scanner(System.in);
 
-        Scanner keyboard = new Scanner(System.in);
-        // boolean booleanpassedHurdle = true;
+    public static void CalculateAndCheckPointsOfStudents() {
 
-        System.out.println("\nInput the numbers of students: ");
-        int numberOfStudents = keyboard.nextInt( );
-        while (numberOfStudents > 0){
-            // processStudentResult
-            numberOfStudents = numberOfStudents -1;
-        }
+        boolean passedHurdle = true;
 
-        System.out.println("\nPlease enter 4 assignments marks and 2 lab marks: ");
-        int assign1 = keyboard.nextInt();
-        int assign2 = keyboard.nextInt();
-        int assign3 = keyboard.nextInt();
-        int assign4 = keyboard.nextInt();
+        System.out.println("Please enter 4 assignments mark and 2 labs mark: ");
+        System.out.print("Assignment 1: ");
+        int assignment1 = keyboard.nextInt();
+        System.out.print("Assignment 2: ");
+        int assignment2 = keyboard.nextInt();
+        System.out.print("Assignment 3: ");
+        int assignment3 = keyboard.nextInt();
+        System.out.print("Assignment 4: ");
+        int assignment4 = keyboard.nextInt();
+        System.out.print("Lab 1: ");
         int lab1 = keyboard.nextInt();
+        System.out.print("Lab 2: ");
         int lab2 = keyboard.nextInt();
-        double pracAverage = (assign1 + assign2 + assign3 + assign4 + lab1 + lab2) / 6.0;
+
+        double pracAverage = (assignment1 + assignment2 + assignment3 + assignment4 + lab1 + lab2) / 6.0;
 
         if (pracAverage >= 50) {
-            System.out.println("Passed assignment/lab hurdle!");
+            System.out.println("Passed assignment / lab hurdle!");
         } else {
             passedHurdle = false;
-            System.out.println("Failed assignment/labhurdle!");
+            System.out.println("Failed assignment / lab hurdle!");
         }
 
         System.out.println("Please enter 2 exam marks: ");
-
+        System.out.print("Exam 1: ");
         int exam1 = keyboard.nextInt();
+        System.out.print("Exam 2: ");
         int exam2 = keyboard.nextInt();
 
         if ((exam1 >= 40) && (exam2 >= 40)) {
@@ -45,17 +46,161 @@ public class SubjectXPass {
             System.out.println("Failed exam hurdle!");
         }
 
-
         double examAverage = (exam1 + exam2) / 2.0;
         double finalMark = 0.3 * pracAverage + 0.7 * examAverage;
-        System.out.println("Final mark is " + finalMark + "%");
+        System.out.println("Final mark is " + finalMark);
 
         if ((finalMark >= 50) && (passedHurdle == true)) {
+
             System.out.println("Passed overall.");
+
         } else {
+
             System.out.println("Failed overall.");
+
         }
 
     }
+
     
+    public static void LimitInputStudent() {
+
+        System.out.println("\n========== Input all of the list of students! ==========");
+        System.out.print("\nInput the numbers of students: ");
+
+        int numberOfStudents = keyboard.nextInt();
+
+        while (numberOfStudents > 0) {
+
+            System.out.println("\nInput the score of student " + numberOfStudents);
+
+            CalculateAndCheckPointsOfStudents();
+
+            numberOfStudents = numberOfStudents - 1;
+
+        }
+
+        System.out.println("You inputed the last of students list!");
+
+        // keyboard.close();
+
+    }
+
+
+    public static void LimitInputStudentLoops() {
+
+        System.out.println("\n========== Do you want to input continue ==========");
+
+        System.out.print("\nInput the numbers of students: ");
+
+        int numberOfStudents = keyboard.nextInt();
+
+        System.out.println("\nInput the score of student " + numberOfStudents);
+
+        CalculateAndCheckPointsOfStudents();
+
+        numberOfStudents = numberOfStudents - 1;
+
+        if (numberOfStudents > 0) {
+
+            do {
+
+                System.out.println("\nDo you want to input continue?");
+                System.out.println("- Press 1 to continue");
+                System.out.println("- Press 0 to exit");
+
+                System.out.print("Your choice: ");
+                int keyStatement = keyboard.nextInt();
+
+                switch (keyStatement) {
+
+                case 1:
+
+                    System.out.println("\nInput the score of student " + numberOfStudents);
+
+                    CalculateAndCheckPointsOfStudents();
+
+                    numberOfStudents = numberOfStudents - 1;
+
+                    break;
+
+                case 0:
+
+                    System.out.println("\nThe program was closed. See you!");
+
+                    keyboard.close();
+
+                    System.exit(1);
+
+                    break;
+
+                default:
+
+                    System.out.println("\nYour choice is invalid, please choose again!");
+
+                    break;
+
+                }
+
+            } while (numberOfStudents > 0);
+
+        } else {
+
+            System.out.println("You inputed the last student!");
+
+        }
+
+        keyboard.close();
+
+    }
+
+
+    public static void UnlimitInputStudentLoops() {
+
+        System.out.println("\n========== Do you want to input continue ==========");
+
+        System.out.println("\nInput the score of student ");
+
+        CalculateAndCheckPointsOfStudents();
+
+        do {
+
+            System.out.println("\nDo you want to input continue?");
+            System.out.println("- Press 1 to continue");
+            System.out.println("- Press 0 to exit");
+
+            System.out.print("Your choice: ");
+            int keyStatement = keyboard.nextInt();
+
+            switch (keyStatement) {
+
+            case 1:
+
+                CalculateAndCheckPointsOfStudents();
+
+                break;
+
+            case 0:
+
+                System.out.println("\nThe program was closed. See you!");
+
+                keyboard.close();
+
+                System.exit(1);
+
+                break;
+
+            default:
+
+                System.out.println("\nYour choice is invalid, please choose again!");
+
+                break;
+
+            }
+
+        } while (true);
+
+    }
+
+
 }
